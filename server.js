4922fcf893d803ac520f2b4df2205cf859f02535
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2022-07-17 22:13:47 -0400

    updated log

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 
 const router = require("./network/routes");
 const db = require("./db");
-console.log("url: ", process.env.MONGO_URL);
 db(process.env.MONGO_URL);
 
 const app = express();
@@ -14,4 +13,4 @@ app.use(bodyParser.json());
 router(app);
 
 app.listen(process.env.PORT || 3000);
-console.log("Server listening in http://localhost:3000");
+console.log(`Server listening in port:${process.env.PORT || 3000}`);
