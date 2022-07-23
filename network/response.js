b9a09c273fc011ce08fdc319a1e45ef54cf81c48
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2022-07-23 15:30:45 -0400

    Update response file

--- network/response.js
+++ network/response.js
@@ -1,18 +1,17 @@
 success = (req, res, message, status = 200) => {
-    res.status(status).send({
-        error: '',
-        body: message
-    })
-}
+  res.status(status).send({
+    items: message,
+  });
+};
 
 error = (req, res, message, status = 500) => {
-    res.status(status).send({
-        error: message,
-        body: ''
-    })
-}
+  res.status(status).send({
+    error: message,
+  });
+};
 
 module.exports = {
-    success,
-    error,
-}
+  success,
+  error,
+};
+
