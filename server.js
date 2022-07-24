46dadc996b6d4d263dac8b45ff2d7fe7ab31c62c
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2022-07-23 21:27:44 -0400

    Updated cors config

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const router = require("./network/routes");
 const db = require("./db");
 db(process.env.MONGO_URL);
-
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(bodyParser.json());
 
 router(app);
