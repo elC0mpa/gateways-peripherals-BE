1ee6aebee0ccda5e40d3cb94e7e547a694dbe130
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2022-08-01 00:06:58 -0400

    Deleted all corresponding peripherals when a gateway is deleted

--- components/gateway/controller.js
+++ components/gateway/controller.js
@@ -1,4 +1,5 @@
 const model = require("./model");
+const peripheralsModel = require("../peripheral/model");
 
 function addGateway(serialNumber, label, address) {
   return new Promise(async (resolve, reject) => {
@@ -56,6 +57,10 @@ async function deleteGateway(id) {
   return new Promise(async (resolve, reject) => {
     try {
       const gateway = await model.findByIdAndDelete(id);
+      gateway.peripherals.length &&
+        (await peripheralsModel.deleteMany({
+          _id: { $in: gateway.peripherals },
+        }));
       resolve(gateway);
     } catch (error) {
       reject(error);
